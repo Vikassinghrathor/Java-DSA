e8fda244188ff3b70c13aefe7a9b0a42da2979cd
vikassinghrathor <rathorevikas2809@gmail.com>
2023-09-26 16:53:13 +0530

    Looping

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,25 @@
-import java.util.*;
-/* Given the Day number, print the Day name in lower case corresponding to it by using the help of switch statement.
-
-Note: Day 1 - is monday.
-
-If the day is not valid example 1> day >7 then print invalid
-
-Input :-
-
-Day - 3
-
-Output :-
-
-wednesday
-
+/* Write a program using only while loops to print all the even numbers from 1 to n
+Input
+if n = 10
+Output
+2
+4
+6
+8
+10
 */
 
+import java.util.*;
 public class Main {
-    public static void weekday_name(int n) {
-        switch(n) {
-            case 1:
-                System.out.println("monday");
-                break;
-            case 2:
-                System.out.println("tuesday");
-                break;
-            case 3:
-                System.out.println("wednesday");
-                break;
-            case 4:
-                System.out.println("thursday");
-                break;
-            case 5:
-                System.out.println("friday");
-                break;
-            case 6:
-                System.out.println("saturday");
-                break;
-            case 7:
-                System.out.println("sunday");
-                break;
-            default:
-                System.out.println("invalid");
-                break;
+    public static void Even_number(int n){
+        int i = 0;
+        while(i <= n){
+            i = i + 2;
+            System.out.println(i);
         }
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        weekday_name(a);
+    public static void main(String[] args){
+        int n = 25;
+        Even_number(n);
     }
 }
