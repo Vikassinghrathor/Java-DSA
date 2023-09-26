839c88e6bb7c66589ad72811551f52e75749b844
vikassinghrathor <rathorevikas2809@gmail.com>
2023-09-26 15:32:52 +0530

    Switch Case

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,53 @@
-/*Write a program to print the largest number of the two numbers given.
-Input:- a=3, b=4
-Output: 4
- */
-
 import java.util.*;
+/* Given the Day number, print the Day name in lower case corresponding to it by using the help of switch statement.
+
+Note: Day 1 - is monday.
+
+If the day is not valid example 1> day >7 then print invalid
+
+Input :-
+
+Day - 3
+
+Output :-
+
+wednesday
+
+*/
+
 public class Main {
-    public static void largest(int x, int y){
-        if(x > y){
-            System.out.println("x is greater");
-        }else{
-            System.out.println("y is greater");
+    public static void weekday_name(int n) {
+        switch(n) {
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+            default:
+                System.out.println("invalid");
+                break;
         }
-
-
     }
 
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number one");
-        int x = input.nextInt();
-        System.out.println("Enter the number two");
-        int y = input.nextInt();
-        largest(x,y);
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        weekday_name(a);
     }
 }
